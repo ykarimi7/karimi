6e1ce90d3e50fa772718286f61f19209a8f87e41
saberqadimi <qadimi43@gmail.com>
2023-11-13 08:40:24 +0330

    merged from git

--- routes/Frontend/Community.php
+++ routes/Frontend/Community.php
@@ -1,7 +1,10 @@
 <?php
+ use Illuminate\Support\Facades\Route;
+ 
 /**
  * Created by NiNaCoder.
  * Date: 2019-08-01
  * Time: 20:33
  */
+
 Route::get('community', 'CommunityController@index')->name('community');
